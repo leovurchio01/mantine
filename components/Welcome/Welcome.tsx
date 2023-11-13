a32299685aab5c549829beb765a07bdd69f84f5b
Leonardo Vurchio <leovurchio01@gmail.com>
2023-11-13 14:38:28 +0100

    mantine

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,18 +5,17 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
+        Hi, I'm {' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+          Leonardo
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
+        contact me on Instagram{' '}
         <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
+          @leovurchio
         </Anchor>
-        . To get started edit page.tsx file.
+        <br></br>See you soon!
       </Text>
     </>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,13 @@ const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
+
 export default withBundleAnalyzer({
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
+  
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
